23c4a3b27c119f56197ae07d78b4add6cc7fd679
Robert Dzikowski <rdzikowski@live.com>
2019-12-11 01:57:10 +0100

    Initial commit

---
+++ init_latlng.js
@@ -0,0 +1,36 @@
+//<![CDATA[
+
+const WARSAW = { lat: 52.229639, lng: 21.011353 };
+const BAGDER_MAPS = { lat: 37.172730, lng: -3.599667 };
+
+var mapOptions = {
+    zoom: 17,
+    center: BAGDER_MAPS,
+    mapTypeId: 'hybrid'
+};
+var map;
+
+function initMap() {
+
+  map = new google.maps.Map(document.getElementById('my_map'), mapOptions);
+
+  var infowindow = new google.maps.InfoWindow({
+        content: "dummy"
+  });
+
+  google.maps.event.addListener(
+        map,
+        'click',
+        function (event) {
+            infowindow.setContent("<strong>lat: </strong>" + event.latLng.lat()
+                + "<br>"
+                + "<strong>lng: </strong>" + event.latLng.lng());
+            infowindow.setPosition(event.latLng);
+            infowindow.open(map);
+  });
+}//initMap()
+
+window.onload = initMap;
+//window.onunload = GUnload;
+
+//]]>
